0bbb16f7e4881a13beb55a643ed9e5a3affd5c7e
Haris Sabanovic <hasa2303@student.miun.se>
2024-02-15 15:42:54 +0100

    animering

--- src/js/main.js
+++ src/js/main.js
@@ -2,9 +2,13 @@
 
 let openBtn = document.getElementById("open-menu");
 let closeBtn = document.getElementById("close-menu");
+let animationBtn = document.getElementById("animation-bt");
 
 openBtn.addEventListener("click", toggleMenu);
 closeBtn.addEventListener("click", toggleMenu);
+animationBtn.addEventListener("click", toggleAnimation);
+
+
 
 function toggleMenu() {
     let navMenuEl = document.getElementById("nav-menu");
@@ -18,4 +22,19 @@ function toggleMenu() {
         navMenuEl.style.display = "none";
     }
 
+}
+
+function toggleAnimation() {
+    console.log("De fungerar");
+
+    let animationEl = document.getElementById("animation");
+
+    let style = window.getComputedStyle(animationEl);
+
+    if(style.display === "none") {
+        animationEl.style.display = "block";
+    }
+    else {
+        animationEl.style.display = "none";
+    }
 }
