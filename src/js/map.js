e377091a8685f3037a3a36d272c1132d7df9d502
Haris Sabanovic <hasa2303@student.miun.se>
2024-02-26 15:08:17 +0100

    lade till en mörkare tile layer

--- src/js/map.js
+++ src/js/map.js
@@ -11,7 +11,13 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-
+var Stadia_AlidadeSmoothDark = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.{ext}', {
+	minZoom: 0,
+	maxZoom: 20,
+	attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	ext: 'png'
+});
+Stadia_AlidadeSmoothDark.addTo(map);
 
 //klick på sök knapp
 searchButtonEl.addEventListener("click", displayLocation);
@@ -27,9 +33,9 @@ function displayLocation() {
             let latitude = data[0].lat;
             let longitude = data[0].lon;
 
-            map.setView([latitude, longitude], 14);
-
-            let singleMarker = L.marker([latitude, longitude]).addTo(map);;
+            map.setView([latitude, longitude], 16);
+            
+            let singleMarker = L.marker([latitude, longitude]).addTo(map);
 
             mapEl.src = `https://www.openstreetmap.org/export/embed.html?bbox=${longitude},${latitude},${longitude},${latitude}&layer=mapnik`;
           })
@@ -38,7 +44,6 @@ function displayLocation() {
             console.log(error);
           })
 };
-
 
 
 //OSM Layer
